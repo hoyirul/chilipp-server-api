114643216856b28dc3dfe166b37f64d43a6a4d5d
Mochammad Hairullah <irulgraspama621@gmail.com>
2022-07-16 20:30:29 +0700

    Update Migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nama');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text('alamat')->nullable();
+            $table->enum('role', ['admin', 'pengepul', 'pasar', 'petani'])->default('petani');
             $table->rememberToken();
             $table->timestamps();
         });
