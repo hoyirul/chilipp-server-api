9d9633051d5bb3c011a972fb3aac510d4f08506d
Mochammad Hairullah <irulgraspama621@gmail.com>
2022-07-16 20:39:30 +0700

    Update Migration

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\CityController;
+use App\Http\Controllers\api\DatasetController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::apiResource('city', CityController::class);
+Route::get('/count', [CityController::class, 'count']);
+
+Route::controller(DatasetController::class)->group(function(){
+    Route::prefix('dataset')->group(function(){
+        Route::get('/', 'index');
+        Route::get('sort_permintaan/{berita}', 'sort_permintaan');
+        Route::get('sort_ketersediaan/{berita}', 'sort_ketersediaan');
+        Route::get('sort_harga/{berita}', 'sort_harga');
+        Route::get('probabilitas_kelas', 'probabilitas_kelas');
+    });
 });
